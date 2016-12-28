e3d034c36584b79fedb5daf3b0087bbbc2e26049
GodJungGun <jg0428.lim@gmail.com>
2016-12-28 15:34:22 +0900

    member, contact, gallery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,27 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def member
+    @members = [{name: "김경도", major: "재료공학부"},
+                {name: "김권봉", major: "시각디자인과"},
+                {name: "김근영", major: "자유전공학부"},
+                {name: "김태완", major: "전기정보공학부"},
+                {name: "박승환", major: "기계항공공학부"},
+                {name: "서동욱", major: "재료공학부"},
+                {name: "서수빈", major: "산업공학과"},
+                {name: "손병찬", major: "전기정보공학부"},
+                {name: "송기서", major: "시각디자인과"},
+                {name: "송재윤", major: "경영학부"},
+                {name: "유진영", major: "경영학부"},
+                {name: "윤현영", major: "자유전공학부"},
+                {name: "이현민", major: "경제학부"},
+                {name: "임정건", major: "화학생물공학부"}]
+  end
+
+  def gallery
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'home/index'
+  get '/member' => 'home#member'
+  get '/gallery' => 'home#gallery'
+  get '/contact' => 'home#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

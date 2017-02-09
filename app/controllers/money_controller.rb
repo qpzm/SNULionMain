d8b1468967ed14c382bc9a4000fb3dde422dca48
NamHyeongKeol <grey_pastor@naver.com>
2017-02-10 00:46:08 +0900

    devise 뼈대 완성

--- app/controllers/money_controller.rb
+++ app/controllers/money_controller.rb
@@ -1,5 +1,6 @@
 class MoneyController < ApplicationController
-#  before_action :authenticate_user! # TODO devise
+  # before_action :authenticate_user!
+  # before_action :authenticate_admin!
     def money_check
     end
 end

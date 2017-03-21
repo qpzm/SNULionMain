f8890dfb61cdc6ecde1e0174ba6d3f6cb8eeb079
Dong Uk <uk6342@gmail.com>
2017-03-21 14:48:27 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get 'money/money_check'
   get 'lecture/lecture'
 
-  devise_for :users, controllers: { sessions: 'users/sessions', passwords: 'users/passwords', registrations: 'users/registrations' }
-  devise_for :admins, controllers: { sessions: 'admins/sessions', passwords: 'admins/passwords', registrations: 'admins/registrations' }
+  devise_for :users
+  devise_for :admins
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

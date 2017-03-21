dbcd408c71eb15b0df0952955a5ceae472e412cf
Dong Uk <uk6342@gmail.com>
2017-03-21 15:42:04 +0900

    model, uploader

--- app/models/submitted_homework.rb
+++ app/models/submitted_homework.rb
@@ -1,4 +1,5 @@
 class SubmittedHomework < ApplicationRecord
+  mount_uploader :img, UploaderUploader
   belongs_to :homework
   belongs_to :user
 end
--- db/migrate/20170321061708_create_submitted_homeworks.rb
+++ db/migrate/20170321061708_create_submitted_homeworks.rb
@@ -4,6 +4,8 @@ class CreateSubmittedHomeworks < ActiveRecord::Migration[5.0]
 
       t.references :homework, index: true, foreign_key: true
       t.references :user, index: true, foreign_key: true
+      t.string :title
+      t.string :img
 
       t.timestamps
     end

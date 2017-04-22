a370376e745457aae3522a249814646953d479a2
NamHyeongKeol <grey_pastor@naver.com>
2017-04-22 22:26:39 +0900

    공백 메시지 안가도록 수정

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -8,8 +8,10 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   def speak (data)
-    m = Message.create(user_or_admin_id: current_user[:user]["id"], user_or_admin_type: current_user[:type], content: data['message'])
-    ActionCable.server.broadcast('messages', message: render_message(m))
+    if data['message'].present?
+      m = Message.create(user_or_admin_id: current_user[:user]["id"], user_or_admin_type: current_user[:type], content: data['message'])
+      ActionCable.server.broadcast('messages', message: render_message(m))
+    end
   end
 
   private

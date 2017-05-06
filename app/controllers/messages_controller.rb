687915c46932cd8aa3e67ea09acadfd3cfa149f1
NamHyeongKeol <grey_pastor@naver.com>
2017-05-06 15:13:09 +0900

    message 하루단위에서 전체 다보이게

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   before_action :authenticate_any!
 
   def index
-    @messagesToday = Message.where('created_at >= ?', DateTime.now.beginning_of_day)
+    # @messagesToday = Message.where('created_at >= ?', DateTime.now.beginning_of_day)
+    @messagesToday = Message.all
   end
 end
